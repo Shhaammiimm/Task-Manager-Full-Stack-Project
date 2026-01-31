2a53ed340b2a6b5346ae28a54b253f58ab50b299
Shhaammiimm <shamim20517@gmail.com>
2026-01-31 13:02:30 +0600

    updated deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/Task-Manager-Full-Stack-Project/',
   plugins: [react()],
   server: {
     port: 5173,
